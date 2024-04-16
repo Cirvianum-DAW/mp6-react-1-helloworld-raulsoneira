fd55c8758a26dd5f30432fdef886a004daad6b93
Raul Soneira <raul.soneira@cirvianum.cat>
2024-04-16 18:10:27 +0200

    Primer pas de informacio estudian

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HelloWorld from "./components/HelloWorld";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
       <h1>Hola, soc App!</h1>
       <HelloWorld />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,19 @@
+// src/components/Form.jsx
+import React from "react";
+
+
+function Form() {
+    
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <form className="w-1/2">
+        <h1 className="text-3xl font-bold text-center mb-8">
+          Informació Estudiant
+        </h1>
+        
+      </form>
+    </div>
+  );
+}
+
+export default Form;
